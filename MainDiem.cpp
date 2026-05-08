31da73fc5fb4970283454bcc280174afb89b4927
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-08 15:10:39 +0700

    code main

---
+++ MainDiem.cpp
@@ -0,0 +1,14 @@
+#include "Diem.cpp"
+int main () {
+//	Diem a (3, 2);
+//	Diem b (5, 6);
+	Diem a;
+	Diem b;
+	cin>>b;
+	cin>>b;
+	cout<<a;
+	cout<<b;
+	float kqkc = a.khoangcach(b);
+	cout<<"Ket qua khoang cach 2 diem la: "<<kqkc<<endl; 
+	return 0;
+}
