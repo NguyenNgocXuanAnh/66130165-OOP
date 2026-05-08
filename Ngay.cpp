dfae54677fdef8730c790ad2ad60c81d446a63cd
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-08 15:29:43 +0700

    contructor có tham số và sao chép

--- Ngay.cpp
+++ Ngay.cpp
@@ -6,4 +6,14 @@ CDate::CDate(){
 	thang = 4;
 	nam = 2005;
 }
+CDate::CDate(int ngaym, int thangm, int namm){
+	ngay = ngaym;
+	thang = thangm;
+	nam = namm;
+}
+CDate::CDate(const CDate &d){
+	ngay = d.ngay;
+	thang = d.thang;
+	nam = d.nam;
+}
 
