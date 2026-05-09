e70a39154c089dd605131eaf92a18443bc589138
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-09 22:59:25 +0700

    code xong main

--- BT_KeThua1.cpp
+++ BT_KeThua1.cpp
@@ -2,8 +2,9 @@
 #include <string>
 using namespace std;
 class Nguoi {
+	protected:
 	string HoTen;
-	usigned int NamSinh;
+	unsigned int NamSinh;
 	public:
 		void Nhap (){
 			cout<<"Nhap ho ten: ";
@@ -12,28 +13,44 @@ class Nguoi {
 			cin>>NamSinh;
 			cin.ignore();//xoa ki tu enter sau khi nhap ns
 		}
-		void Xuat (){
-			cout<<"Ho ten: "<<HoTen<<endl;
-			cout<<"Nam sinh: "<<NamSinh<<endl;
-		}
 		unsigned int tinhtuoi (){
 			return 2026-NamSinh;
 		}
+		void Xuat (){
+			cout<<"Ho ten: "<<HoTen<<endl;
+			cout<<"Nam sinh: "<<NamSinh<<endl;
+			cout<<"Tuoi: "<<tinhtuoi()<<endl;
+		}
 };
 class SinhVien:public Nguoi{
+	protected:
 	string MSSV;
 	double Diem;
 	public:
-		void Nhap (){
+		void NhapSV (){
 			cout<<"Nhap MSSV: ";
 			getline (cin,MSSV);
 			cout<<"Nhap diem: ";
 			cin>>Diem;
 			cin.ignore();
 		}
-		void Xuat (){
-			xuat();
+		void XuatSV (){
+			Xuat();
 			cout<<"MSSV: "<<MSSV<<endl;
 			cout<<"Diem: "<<Diem<<endl;
 		}
 }; 
+int main (){
+//	Nguoi n1;
+//	n1.Nhap();
+//	n1.Xuat();
+//	n1.tinhtuoi();
+	SinhVien sv1;
+	sv1.Nhap();
+	sv1.tinhtuoi();
+	sv1.NhapSV();
+	cout<<"_________THONG TIN SINH VIEN_________"<<endl;
+	sv1.XuatSV();
+	return 0;
+	
+} 
