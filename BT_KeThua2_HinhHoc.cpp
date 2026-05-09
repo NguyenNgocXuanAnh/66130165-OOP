7f12cd9e36192b96cc38587794f50205825bd55f
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-09 23:09:01 +0700

    tạo lớp hcn kế thừa từ lớp hình học

--- BT_KeThua2_HinhHoc.cpp
+++ BT_KeThua2_HinhHoc.cpp
@@ -13,3 +13,22 @@ class HinhHoc {
 		cout<<"Mau sac: "<<color;
 	}
 }; 
+class HCN:public HinhHoc {
+	protected:
+		double cdai;
+		double crong;
+		void Nhaphcn (){
+			cout<<"Nhap chieu dai: ";
+			cin>>cdai;
+			cout<<"Nhap chieu rong: ";
+			cin>>crong;
+		}
+		double tinhdt (){
+			return cdai*crong;
+		}
+		void Xuat (){
+			cout<<"Chieu dai: "<<cdai<<endl;
+			cout<<"Chieu rong: "<<crong<<endl;
+			cout<<"Dien tich: "<<tinhdt()<<endl;
+		}
+}; 
